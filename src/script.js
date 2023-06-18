8e4b036b4c7d5f8d2b39d7307f3bfa5f4456a2ce
Valkimani <66477953+Valkimani@users.noreply.github.com>
2023-06-18 16:23:49 -0400

    Created a hamburger and navList constants in the script.js file to be allow a user to see the nav bar links when the hamburger icon is clicked.

---
+++ src/script.js
@@ -0,0 +1,4 @@
+// Allow user to see all the other icons when the hamburger icon is clicked.
+
+const hamburgerButton = document.getElementById('humburger')
+const navList= document.getElementById('nav-list')
